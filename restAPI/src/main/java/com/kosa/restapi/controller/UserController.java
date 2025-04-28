942774695b083cc09e91e9e148c0bb8ed477bf07
kimddo1 <dowonkim2004@gmail.com>
2025-04-28 12:29:20 +0900

    springboot last save?

--- restAPI/src/main/java/com/kosa/restapi/controller/UserController.java
+++ restAPI/src/main/java/com/kosa/restapi/controller/UserController.java
@@ -4,6 +4,8 @@ import com.kosa.restapi.domain.User;
 import com.kosa.restapi.exception.UserNotFoundException;
 import com.kosa.restapi.service.UserDaoService;
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -24,17 +26,25 @@ public class UserController {
         return service.findAll();
     }
 
-
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user==null){
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
-        return user;
+
+        EntityModel<User> model = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder
+                .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUser());
+
+        model.add(linkTo.withRel("all-users"));
+
+        return model;
     }
 
+    //HTTP Status 제어
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
--- restAPI/src/main/java/com/kosa/restapi/domain/User.java
+++ restAPI/src/main/java/com/kosa/restapi/domain/User.java
@@ -2,6 +2,7 @@ package com.kosa.restapi.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,8 +15,12 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(value = {"password", "ssn"})
+@Schema(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
+    @Schema(title = "사용자 ID", description = "사용자 ID는 자동 생성 됩니다.")
     private Integer id;
+
+    @Schema(title = "사용자 이름", description = "사용자 이름을 입력하세요:")
     @Size(min = 2, max = 20, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
     @Past(message = "등록일은 미래 날짜를 입력하실 수 있습니다.")
